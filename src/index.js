b34464373eec54d2fd69a0e773132cc2a7e02636
jaomisterio <joaogamerpolado@gmail.com>
2022-11-04 09:19:12 -0300

    change index type

--- src/index.js
+++ src/index.js
@@ -1,89 +1,88 @@
 const fs = require("fs");
 const Discord = require("discord.js");
 const { Client, IntentsBitField } = require("discord.js");
-const mongoose = require('mongoose')
+const mongoose = require("mongoose");
 require("dotenv").config();
 
 const myIntents = new IntentsBitField();
 myIntents.add(
-	IntentsBitField.Flags.Guilds,
-	IntentsBitField.Flags.GuildMessages,
-	IntentsBitField.Flags.GuildMessageReactions,
-	IntentsBitField.Flags.MessageContent,
-	IntentsBitField.Flags.GuildVoiceStates
+  IntentsBitField.Flags.Guilds,
+  IntentsBitField.Flags.GuildMessages,
+  IntentsBitField.Flags.GuildMessageReactions,
+  IntentsBitField.Flags.MessageContent,
+  IntentsBitField.Flags.GuildVoiceStates
 );
+const { token, MONGO_URI, prefix } = require("./config.json");
 
 const client = new Client({ intents: myIntents });
-let { prefix } = require('./config.json')
-client.prefix = process.env.PREFIX || prefix
-client.commands = new Discord.Collection()
+client.prefix = prefix;
+client.commands = new Discord.Collection();
 
 const commandFiles = [];
 
-(findCommands = async (path = 'commands') => {
-	fs.readdirSync(`./src/${path}`).filter(async file => {
-		if (file === 'aliases') return
-		if (file.endsWith('.js')) return commandFiles.push(file)
-		findCommands(path + `/${file}`)
-	})
-})()
+(findCommands = async (path = "commands") => {
+  fs.readdirSync(`./src/${path}`).filter(async (file) => {
+    if (file === "aliases") return;
+    if (file.endsWith(".js")) return commandFiles.push(file);
+    findCommands(path + `/${file}`);
+  });
+})();
 
-const eventFiles = fs.readdirSync('./src/events').filter(file => file.endsWith('.js'));
+const eventFiles = fs
+  .readdirSync("./src/events")
+  .filter((file) => file.endsWith(".js"));
 
 for (const file of commandFiles) {
-	let commander
-	const getCommands = (name, path = 'commands') => {
+  let commander;
+  const getCommands = (name, path = "commands") => {
+    const dirr = `./src/${path}`;
 
-		const dirr = `./src/${path}`
+    fs.readdirSync(dirr).filter((find) => {
+      if (find === name) {
+        commander = `./${path}/${find}`;
+      } else {
+        if (find.endsWith("js") === true) return;
+        if (find === "aliases") return;
+        return getCommands(name, path + `/${find}`);
+      }
+    });
+  };
+  getCommands(file);
 
-		fs.readdirSync(dirr).filter(find => {
-			if (find === name){
-				commander = `./${path}/${find}`
-			}else{
-				if (find.endsWith('js') === true) return 
-				if (find === 'aliases') return
-				return getCommands(name, path + `/${find}`)
-			}
-		})
-	}
-	getCommands(file)
-
-	const command = require(commander)	
-	client.commands.set(command.name === undefined ? command.data.name : command.name, command);
-}	
-
-for (const file of eventFiles) {
-
-	const event = require(`./events/${file}`);
-
-	if (event.once) {
-		client.once(event.name, (...args) => event.execute(...args, client));
-
-	} else {
-		if (event.name === 'voiceStateUpdate') {
-			client.on('voiceStateUpdate', (oldconnect, newconnect) => {
-				event.execute(oldconnect, newconnect, client)
-			})
-		} else {
-			client.on(event.name, data => {
-				if (data.type === 2) {
-					event.execute(data, client, 'slash')
-				}
-				if (data.type === 0) {
-					event.execute(data, client, 'message')
-				}
-			})
-		}
-
-	}
+  const command = require(commander);
+  client.commands.set(
+    command.name === undefined ? command.data.name : command.name,
+    command
+  );
 }
 
-client.login(process.env.BOT_KEY).then(async () => {
-	console.log('Bot está online\n')
-	await mongoose.connect(
-		process.env.MONGO_URI || '', {
-		keepAlive: true,
-	}
-	)
-	console.log('Conectado a DB')
+for (const file of eventFiles) {
+  const event = require(`./events/${file}`);
+
+  if (event.once) {
+    client.once(event.name, (...args) => event.execute(...args, client));
+  } else {
+    if (event.name === "voiceStateUpdate") {
+      client.on("voiceStateUpdate", (oldconnect, newconnect) => {
+        event.execute(oldconnect, newconnect, client);
+      });
+    } else {
+      client.on(event.name, (data) => {
+        if (data.type === 2) {
+          event.execute(data, client, "slash");
+        }
+        if (data.type === 0) {
+          event.execute(data, client, "message");
+        }
+      });
+    }
+  }
+}
+
+client.login(token).then(async () => {
+  console.log("Bot Status: ONLINE\n");
+  await mongoose.connect(MONGO_URI || "", {
+    keepAlive: true,
+  });
+  console.log("\nDataBase connected.");
 });
